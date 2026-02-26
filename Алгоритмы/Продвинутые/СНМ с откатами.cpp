aa49b4d41fff2100f6106525e86bf112907e4dca
Поздняков Ярослав <yariknoob257@gmail.com>
2026-02-26 13:05:58 +0000

    СНМ с откатами

---
+++ Алгоритмы/Продвинутые/СНМ с откатами.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+vector<int> up;
+stack<pair<int, int>> st;
+
+int p(int a) {
+    return up[a] < 0 ? a : p(up[a]);
+}
+void unite(int a, int b) {
+    a = p(a);
+    b = p(b);
+    if (up[a] > up[b]) swap(a, b);
+    st.push({up[b], b});
+    up[a] += up[b];
+    up[b] = a;
+}
+int back() {
+    auto a = st.top();
+    st.pop();
+    up[p(a.second)] -= a.first;
+    up[a.second] = a.first;
+    return a.second;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    long long n, m;
+    cin >> n >> m;
+    up.assign(n, -1);
+    for (int i = 0; i < m; i++) {
+        char a;
+        cin >> a;
+        if (a == '-') {
+            cout << -up[back()] << endl;
+        }
+        if (a == '+') {
+            int b, c;
+            cin >> b >> c;
+            unite(b, c);
+            cout << -up[b] << endl;
+        }
+        if (a == '?') {
+            int b;
+            cin >> b;
+            cout << -up[p(b)] << endl;
+        }
+    }
+}
